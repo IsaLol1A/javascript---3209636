4b02a645da84f3da82fa3ce6661d009a7f5ba433
chofi <123charaagudelo@gmail.com>
2025-10-21 15:24:37 -0500

    Add files via upload

--- Poster-Interactivo/app.js
+++ Poster-Interactivo/app.js
@@ -1,3 +1,37 @@
+//1. capturar variables
+const corazon = document.querySelectorAll('.oli ');
 const moneda = document.querySelectorAll('.viktor');
+const counter = document.querySelector('.contador');
+let i = 0;
 
-/* const counter = document.querySelector('.counter'); */
+console.log(corazon);
+console.log(moneda);
+console.log(counter);
+
+//2. crear las funciones
+
+moneda.forEach(Item => {
+   // console.log("Elemento: ", Item);
+   
+    Item.addEventListener('click', () => {
+         // Item.style.filter = "grayscale(1)";
+         Item.classList.add('saltar');
+
+    i++;
+    counter.textContent = `ESCORE: ${i}`;
+    }) })
+
+   /* Item.addEventListener('animationend', () => {
+        Item.style.display = "none";
+    }, { once: true })*/
+
+corazon.forEach(Item => {
+
+    Item.addEventListener('click', () => {
+        Item.style.filter = "grayscale(1)";
+    })
+})
+
+
+
+//3. llamar eventos
