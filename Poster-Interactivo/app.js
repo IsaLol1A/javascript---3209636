213da282161dc927d510232b68d8dff9e0ffa3e4
chofi <123charaagudelo@gmail.com>
2025-10-24 20:17:38 -0500

    Add files via upload
    Tres escenarios con animacion

--- Poster-Interactivo/app.js
+++ Poster-Interactivo/app.js
@@ -6,8 +6,6 @@ let i = 0;
 console.log(moneda);
 console.log(counter);
 
-
-
 moneda.forEach(Item => {
    // console.log("Elemento: ", Item);
    
@@ -23,20 +21,40 @@ moneda.forEach(Item => {
         Item.style.display = "none";
     }, { once: true })*/
 
-coco.forEach(Item => {
-    Item.addEventListener('click', () => {
-        Item.style.filter = "grayscale(1 )";
-    }) })
+
 
 
 
 //Escenario 2
+const counter2 = document.querySelector('.contador2');
 const manzana = document.querySelectorAll('.mamoi');
+let j = 0;
 
+console.log(counter2);
 console.log(manzana);
+
 
 manzana.forEach(Item => {
      
     Item.addEventListener('click', () => {
-         Item.classList.add('saltarM'); 
+        Item.classList.add('saltarM');
+        j++;
+        counter2.textContent = `ESCORE: ${j}`;
+    }) })
+
+
+
+//Escenario 3
+const diamante = document.querySelectorAll('.julio');
+const counter3 = document.querySelector('.contador3');
+let k = 0;
+
+console.log(diamante);
+console.log(counter3);
+
+diamante.forEach(Item => {
+    Item.addEventListener('click', () => {
+        Item.classList.add('saltarD');
+        k++;
+        counter3.textContent = `ESCORE: ${k}`;
     }) })
