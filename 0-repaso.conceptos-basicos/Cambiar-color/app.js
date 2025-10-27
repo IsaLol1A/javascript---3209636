0404fe1aadf50bb3910929b1de17db7710319387
chofi <123charaagudelo@gmail.com>
2025-10-27 15:25:18 -0500

    Add files via upload

---
+++ 0-repaso.conceptos-basicos/Cambiar-color/app.js
@@ -0,0 +1,16 @@
+let boton = document.querySelector(".boton")
+let body = document.querySelector(".body")
+
+boton.addEventListener("click", cambiarColorFondo);
+
+function cambiarColorFondo(){
+
+if( body.style.backgroundColor === "black"){
+    body.style.backgroundColor = "white"
+    body.style.color = "black";
+} else{
+    body.style.backgroundColor = "black"
+    body.style.color = "white";
+    boton.style.backgroundColor = "black"
+    boton.style.color = "white"
+} }
