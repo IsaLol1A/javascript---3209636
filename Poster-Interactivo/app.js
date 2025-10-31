2d8c18734deefec8443611279868353748f28f1d
chofi <123charaagudelo@gmail.com>
2025-10-31 15:57:37 -0500

    funcion mostrar escena (a medias)

--- Poster-Interactivo/app.js
+++ Poster-Interactivo/app.js
@@ -7,21 +7,20 @@ console.log(moneda);
 console.log(counter);
 
 moneda.forEach(Item => {
-   // console.log("Elemento: ", Item);
-   
+    // console.log("Elemento: ", Item);
+
     Item.addEventListener('click', () => {
-         // Item.style.filter = "grayscale(1)";
-         Item.classList.add('saltar');
+        // Item.style.filter = "grayscale(1)";
+        Item.classList.add('saltar');
 
-    i++;
-    counter.textContent = `ESCORE: ${i}`;
-    }) })
+        i++;
+        counter.textContent = `ESCORE: ${i}`;
+    })
+})
 
-   /* Item.addEventListener('animationend', () => {
-        Item.style.display = "none";
-    }, { once: true })*/
-
-
+/* Item.addEventListener('animationend', () => {
+     Item.style.display = "none";
+ }, { once: true })*/
 
 
 
@@ -35,12 +34,13 @@ console.log(manzana);
 
 
 manzana.forEach(Item => {
-     
+
     Item.addEventListener('click', () => {
         Item.classList.add('saltarM');
         j++;
         counter2.textContent = `ESCORE: ${j}`;
-    }) })
+    })
+})
 
 
 
@@ -57,4 +57,36 @@ diamante.forEach(Item => {
         Item.classList.add('saltarD');
         k++;
         counter3.textContent = `ESCORE: ${k}`;
-    }) })
+    })
+})
+
+
+/*Carrusel wAaAaAaaaaa*/
+
+//1.Variables
+/*
+const escenario = document.querySelectorAll('.escena');
+const siguientes = document.querySelector('.siguiente');
+const anterior = document.querySelector('.anterior');
+const miniatura = document.querySelectorAll('.poio');
+
+let indice = 0;
+
+console.log(escenario);
+console.log(siguientes);
+console.log(anterior);
+console.log(miniatura);
+
+
+//2. Funciones
+function mostrarEscenario(i) {
+    for (let j = 0; j < escenario.length; j++) {
+        escenario[j].classList.remove("activa")
+    }
+    escenario[i].classList.add("activa");
+}
+
+/* mostrarEscenario(2); */
+
+
+//3. Eventos
